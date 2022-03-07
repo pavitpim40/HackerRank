71cad37cbeb495e643b176e96eab8fc93d7bcf6a
Pavit Pimchanagul <63455126+pavitpim40@users.noreply.github.com>
2022-03-07 12:25:39 +0700

    Update other solution and rename

--- 10_Bitwise-Operator.c
+++ 10_Bitwise-Operator.c
@@ -39,3 +39,11 @@ int main() {
  
     return 0;
 }
+
+
+// other Solution
+void calculate_the_maximum(int n, int k) {
+    printf("%d\n",((k-1)|k)<=n? (k-1):(k-2));
+    printf("%d\n",((k-1)&(k-2))==0? (k==3?0:(k-2)) : (k-1) );
+    printf("%d\n", k-1);
+}
